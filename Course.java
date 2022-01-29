619f0123e04f4cad5509cab45cd91e250453e011
Aslinur Unal <96949096+aslinurunal@users.noreply.github.com>
2022-01-29 23:45:52 +0300

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,36 @@
+package classes;
+
+public class Course {
+	Teacher courseTeacher;
+	String name;
+	String code;
+	String prefix;
+	int note;
+	int oral;
+
+	public Course(String name, String code, String prefix) {
+		this.name = name;
+		this.code = code;
+		this.prefix = prefix;
+		this.note = 0;
+		this.oral = 0;
+	}
+
+	public void addTeacher(Teacher t) {
+		if (this.prefix.contentEquals(t.branch)) {
+			this.courseTeacher = t;
+			System.out.println("Ýþlem Baþarýlý");
+		} else {
+			System.out.println(t.name + " Akademisyeni bu derse verilemez");
+		}
+	}
+
+	public void printTeacher() {
+		if (courseTeacher != null) {
+			System.out.println(this.name + " dersinin Akademisyeni: " + courseTeacher.name);
+		} else {
+			System.out.println(this.name + " dersine Akademisyen atanmamýþtýr");
+		}
+	}
+
+}
